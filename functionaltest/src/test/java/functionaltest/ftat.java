d4168d895ba119190c11efaebf783cc922d0da0e
Sivaraman <saru00siva@gmail.com>
2020-04-08 15:57:08 -0600

    Checking in functional test

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -18,9 +18,6 @@ public class ftat {
 	public WebDriverWait wait;
 	public String URL = "http://18.191.229.168:8080/Registrationapp/";
 	public String URL1 = "http://18.191.229.168:8080/Registrationapp/login";
-	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
-	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
-	 
 	 
 	@Test(priority = 0)
 	public void homePagetest() {
